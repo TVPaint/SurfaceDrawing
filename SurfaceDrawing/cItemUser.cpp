8cc151c53018273e964f1b4365f0e50c8195bb36
Damien <dlehmann@tvpaint.fr>
2018-11-13 11:00:59 +0100

    lipstick

--- SurfaceDrawing/cItemUser.cpp
+++ SurfaceDrawing/cItemUser.cpp
@@ -29,8 +29,24 @@ cItemUser::paint( QPainter * iPainter, const QStyleOptionGraphicsItem * iOptions
     if( mUser->mIsDead )
         return;
 
-    iPainter->setBrush( QBrush( mUser->mColor ) );
-    iPainter->setPen( QPen( mUser->mColor ) );
+    auto size = mUser->mGUISize;
+    QRadialGradient grad( mUser->mGUISize/2, mUser->mGUISize.x(), mUser->mGUISize/2 );
+    grad.setColorAt( 0, mUser->mColor );
+    grad.setColorAt( 1, mUser->mColor.darker( 120 ) );
+
+    iPainter->setBrush( grad );
+    iPainter->setPen( QPen( Qt::black ) );
     iPainter->drawRect( boundingRect() );
+    return;
+
+    //auto size = mUser->mGUISize;
+    //QLinearGradient grad( QPoint( 0, 0 ), mUser->mGUISize );
+    //grad.setColorAt( 0, mUser->mColor );
+    //grad.setColorAt( 1, mUser->mColor.lighter( 200 ) );
+
+    //iPainter->setBrush( grad );
+    //iPainter->setPen( QPen( Qt::black ) );
+    //iPainter->drawRect( boundingRect() );
+
 }
 
