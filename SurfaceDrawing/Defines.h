373c1dc917c79cb5f137c15c07c0c1c5e6b04ef0
Damien <dlehmann@tvpaint.fr>
2018-11-14 17:00:32 +0100

    comment

--- SurfaceDrawing/Defines.h
+++ SurfaceDrawing/Defines.h
@@ -6,7 +6,7 @@
 #define     SPEED                   20          // ms for 1 pixel
 #define     CLOCKTIME               108000000   // 30 min
 #define     SPAWNINGAREAREQUIRED    5           // 5x5 ( spawns are 3x3, here we let a little room )
-#define     ROLLBACKSPEED           2
-#define     SPEEDBOOST              3
+#define     ROLLBACKSPEED           2           // Multiplier of speed
+#define     SPEEDBOOST              3           // Multiplier of speed
 
 #define     UICOMPSIZE              32
